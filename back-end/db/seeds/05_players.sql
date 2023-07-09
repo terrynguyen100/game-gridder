45522d73d71b8ce93a5e7e52e9b2d5ea6bb09590
Clayton <persinger.clayton@gmail.com>
2023-07-08 18:44:47 -0700

    Update seed to display correct name

--- back-end/db/seeds/05_players.sql
+++ back-end/db/seeds/05_players.sql
@@ -7,10 +7,10 @@ INSERT INTO players (user_id, match_id, player_name, score) VALUES (6, 3, 'Emily
 INSERT INTO players (user_id, match_id, player_name, score) VALUES (7, 4, 'Daniel Wilson', '3');
 INSERT INTO players (user_id, match_id, player_name, score) VALUES (8, 4, 'Olivia Anderson', '2');
 
-INSERT INTO players (user_id, match_id, player_name, score) VALUES (1, 5, 'Matthew Martinez', '3');
-INSERT INTO players (user_id, match_id, player_name, score) VALUES (4, 5, 'Sophia Thompson', '1');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (1, 5, 'John Doe', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (4, 5, 'Sarah Williams', '1');
 INSERT INTO players (user_id, match_id, player_name, score) VALUES (5, 6, 'Michael Brown', '3');
 INSERT INTO players (user_id, match_id, player_name, score) VALUES (7, 6, 'Daniel Wilson', '0');
 
-INSERT INTO players (user_id, match_id, player_name, score) VALUES (1, 7, 'Matthew Martinez', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (1, 7, 'John Doe', '3');
 INSERT INTO players (user_id, match_id, player_name, score) VALUES (5, 7, 'Michael Brown', '2');
