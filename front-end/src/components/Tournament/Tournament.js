1ce41bc739a8a30e2c215de55527ba5b51800fd9
Clayton <persinger.clayton@gmail.com>
2023-07-11 07:50:00 -0700

    Create logic for displaying how many rounds there will be

--- front-end/src/components/Tournament/Tournament.js
+++ front-end/src/components/Tournament/Tournament.js
@@ -9,7 +9,11 @@ import "./../../sass/tournament.scss"
 const Tournament = ({ tournament, numOfPlayers }) => {
 
   const tournamentDate = `${new Date(tournament.start_date).toString().slice(0, 16)} at ${new Date(tournament.start_date).toString().slice(16, 28)}`
-  console.log(tournament)
+  // console.log(tournament)
+
+  //Determine how many rounds there will be
+  let factorOf2 = 0
+  while (Math.pow(2, factorOf2) < numOfPlayers) factorOf2++;
 
   return (
     <>
@@ -17,8 +21,8 @@ const Tournament = ({ tournament, numOfPlayers }) => {
         Hello
       </h1>
       <div id="tournament-info">
-        <h1 class="title">{tournament.name}</h1>
-        <div class="tournament-stats">
+        <h1 className="title">{tournament.name}</h1>
+        <div className="tournament-stats">
           <Typography variant="body2" color="text.secondary" sx={{display: "flex", alignItems: "center", color: "#FFF"}}>
             <PersonIcon sx={{mr: 1.5}}/> 
             <span>{numOfPlayers}</span>
@@ -37,24 +41,29 @@ const Tournament = ({ tournament, numOfPlayers }) => {
           </Typography>
         </div>
       </div>
-      <div id="tournament-bracket">
 
+      <div id="tournament-bracket">
+        <div className="tournament-rounds">
+          {[...Array(factorOf2)].map((e, i) => 
+              <div className="round-details" key={i}>Round {i + 1}<br/><span className="date">{new Date(tournament.start_date).toString().slice(0, 10)}</span></div>
+          )}
+        </div>
+      
         {tournament.matches.map(match => {
           return(
             <>
-            <section id="bracket">
-              <div class="container">
-                <div class="split split-one">
-                  <div class="round round-one current">
-                    <div class="round-details">Round 1<br/><span class="date">{new Date(tournament.start_date).toString().slice(0, 10)}</span></div>
-                    <ul class="matchup">
-                      <li class="team team-top">{match.players[0].player_name}<span class="score">{match.players[0].score}</span></li>
-                      <li class="team team-bottom">{match.players[1].player_name}<span class="score">{match.players[1].score}</span></li>
-                    </ul>
+              <section id="bracket">
+                <div className="container">
+                  <div className="split split-one">
+                    <div className="round round-one current">
+                      <ul className="matchup">
+                        <li className="team team-top">{match.players[0].player_name}<span className="score">{match.players[0].score}</span></li>
+                        <li className="team team-bottom">{match.players[1].player_name}<span className="score">{match.players[1].score}</span></li>
+                      </ul>
+                    </div>  
                   </div>  
                 </div>  
-              </div>  
-            </section >
+              </section >
             </>
           )
         })}
