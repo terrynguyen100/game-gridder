079d8ece6da75929816ba6b23b6f01ce25ae2433
Clayton <persinger.clayton@gmail.com>
2023-07-10 18:43:20 -0700

    Add handle route funciton and conditionals to display tournaments

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,13 +6,24 @@ import Login from './components/Login';
 import UserProfile from './components/UserProfile';
 import { RouteContext } from './providers/RouteProvider';
 
+import Tournaments from './components/Tournaments/Tournaments';
+import Tournament from './components/Tournament/Tournament';
+
 function App() {
-  const { route } = useContext(RouteContext);
+  const { route, changeRoute } = useContext(RouteContext);
   const [login, setLogin] = useState(false);
+  const [t, setT] = useState({})
+
+  const handleRoute = ({ tournament, numOfPlayers}) => {
+    changeRoute(`/tournament/${tournament.id}`);
+    setT({tournament, numOfPlayers})
+  }
 
   return (
     <div className="App">
         <Navbar login={login} />
+        {t.tournament && ((route === `/tournament/${t.tournament.id}`) && <Tournament tournament={t.tournament} numOfPlayers={t.numOfPlayers}/>)}
+        <Tournaments handleRoute={handleRoute}/> 
         {(route === '/') && <Home />}
         {(route === '/login') && <Login />}
     </div>
