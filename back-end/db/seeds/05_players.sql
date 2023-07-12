6f39fc31383914686daf7cc98be352fa0973b04f
Clayton <persinger.clayton@gmail.com>
2023-07-11 20:21:26 -0700

    Add new seeds so a second and third tournament could be generated

--- back-end/db/seeds/05_players.sql
+++ back-end/db/seeds/05_players.sql
@@ -1,3 +1,4 @@
+-- Tournament 1
 INSERT INTO players (user_id, match_id, player_name, score) VALUES (1, 1, 'John Doe', '3');
 INSERT INTO players (user_id, match_id, player_name, score) VALUES (2, 1, 'Jane Smith', '1');
 INSERT INTO players (user_id, match_id, player_name, score) VALUES (3, 2, 'David Johnson', '1');
@@ -14,3 +15,51 @@ INSERT INTO players (user_id, match_id, player_name, score) VALUES (7, 6, 'Danie
 
 INSERT INTO players (user_id, match_id, player_name, score) VALUES (1, 7, 'John Doe', '3');
 INSERT INTO players (user_id, match_id, player_name, score) VALUES (5, 7, 'Michael Brown', '2');
+
+-- Tournament 2
+
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (1, 8, 'John Doe', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (2, 8, 'Jane Smith', '1');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (3, 9, 'David Johnson', '1');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (4, 9, 'Sarah Williams', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (5, 10, 'Michael Brown', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (6, 10, 'Emily Davis', '0');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (7, 11, 'Daniel Wilson', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (8, 11, 'Olivia Anderson', '2');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (9, 12, 'Matthew Martinez', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (10, 12, 'Sophia Thompson', '1');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (11, 13, 'Benjamin Harris', '1');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (12, 13, 'Ava Clark', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (13, 14, 'Jacob Young', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (14, 14, 'Mia Lee', '0');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (15, 15, 'William Garcia', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (16, 15, 'AbigailMartin', '2');
+
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (1, 16, 'John Doe', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (4, 16, 'Sarah Williams', '1');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (5, 17, 'Michael Brown', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (7, 17, 'Daniel Wilson', '0');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (9, 18, 'Matthew Martinez', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (12, 18, 'Ava Clark', '1');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (13, 19, 'Jacob Young', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (15, 19, 'Daniel Wilson', '0');
+
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (1, 20, 'John Doe', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (5, 20, 'Michael Brown', '2');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (12, 21, 'Ava Clark', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (15, 21, 'William Garcia', '2');
+
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (1, 22, 'John Doe', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (12, 22, 'Ava Clark', '2');
+
+-- Tournament 3
+
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (1, 23, 'John Doe', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (4, 23, 'Sarah Williams', '1');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (5, 24, 'Michael Brown', '3');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (7, 24, 'Daniel Wilson', '0');
+
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (4, 25, 'Sarah Williams', '1');
+INSERT INTO players (user_id, match_id, player_name, score) VALUES (5, 25, 'Michael Brown', '3');
+
+
