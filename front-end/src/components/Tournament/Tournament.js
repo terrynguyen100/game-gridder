e4559dd8852230a747b54a4e8d42744ea4b40201
Clayton <persinger.clayton@gmail.com>
2023-07-14 10:49:33 -0700

    Revamp file to use async await, remove bracket code to  its own component

--- front-end/src/components/Tournament/Tournament.js
+++ front-end/src/components/Tournament/Tournament.js
@@ -13,6 +13,7 @@ import top_match_down from './svg_files/top_match_down';
 import bottom_match_up from './svg_files/bottom_match_up';
 import connector from './svg_files/connector';
 import straight_line from './svg_files/straight_line';
+import Bracket from './Bracket';
 
 const Tournament = () => {
   const [tournament, setTournament] = useState(null);
@@ -26,57 +27,58 @@ const Tournament = () => {
 
   // Need proper cleanup to work in this file, otherwise app crashes from too many requests
   useEffect(() => {
-    let isMounted = true;
-    axios.get(`/tournaments/${tournamentID.id}`)
-    .then((response) => {
-      if (isMounted) {
-      setTournament(response.data)
-      if(response.data.matches.length === 0) setNumOfPlayers([].length)
-    const getPlayerNums = response.data.matches.reduce((ac, cv) => {
-      if(cv.players.length === 0) return ac
-      if(!ac.includes(cv.players[0].player_name)) ac.push(cv.players[0]?.player_name)
-      if(!ac.includes(cv.players[1].player_name)) ac.push(cv.players[1]?.player_name)
-      return ac
-    }, [])
-    setNumOfPlayers(getPlayerNums.length);
-
-    setTournamentDate(`${new Date(response.data.start_date).toString().slice(0, 16)} at ${new Date(response.data.start_date).toString().slice(16, 28)}`)
-
-    //Determine how many rounds there will be
-    while (Math.pow(2, numOfRounds) < numOfPlayers) setNumOfRounds((prev) => prev + 1);
-  
-    // This is not spreading properly on line 111. If spread operator is used, all matches are stacked on top of each other. Put it inside useEffect since it needs the numOfPlayers and numOfRounds and the API call needs to happen before those can be defined.
-      setBracketStyle({
-      display: "grid",
-      gridTemplateRows: `repeat(${numOfPlayers - 1}, 60px)`,
-      gridTemplateColumns: `repeat(${(2 * numOfRounds) + (numOfRounds - 1)}, 1fr)`,
-      gridTemplateAreas: generateTemplateAreas(numOfPlayers, numOfRounds),
-      justifyContent: "center",
-      alignItems: "stretch"
-    });
-  
-    if( numOfPlayers < 8) {
-      setBracketWidth({
-        width: "50%"
-      })
-    } else if ( numOfPlayers < 16) {
-      setBracketWidth({
-        width: "75%"
-      })
-    } else if ( numOfPlayers < 32) {
-      setBracketWidth({
-        width: "90%"
-      })
-    }
-    }})
-
-    return () => {
-      isMounted = false;
-    };
-  }, [])
+    setData()
+  }, [numOfPlayers])
 
 
-  if (tournament !== null) {
+  const setData = async() => {
+    const tempData = await axios.get(`/tournaments/${tournamentID.id}`)
+
+        setTournament(tempData.data)
+        if(tempData.data.matches.length === 0) setNumOfPlayers([].length)
+        const getPlayerNums = tempData.data.matches.reduce((ac, cv) => {
+          if(cv.players.length === 0) return ac
+          if(!ac.includes(cv.players[0].player_name)) ac.push(cv.players[0]?.player_name)
+          if(!ac.includes(cv.players[1].player_name)) ac.push(cv.players[1]?.player_name)
+          return ac
+        }, [])
+        setNumOfPlayers(getPlayerNums.length);
+
+        setTournamentDate(`${new Date(tempData.data.start_date).toString().slice(0, 16)} at ${new Date(tempData.data.start_date).toString().slice(16, 28)}`)
+
+
+        //Determine how many rounds there will be
+        if(numOfPlayers !== 0) {
+          const numOfRoundsHolder = Math.ceil(Math.log2(numOfPlayers));
+          setNumOfRounds(numOfRoundsHolder);
+        }
+
+        if( numOfPlayers < 8) {
+          setBracketWidth({
+            width: "50%"
+          })
+        } else if ( numOfPlayers < 16) {
+          setBracketWidth({
+            width: "75%"
+          })
+        } else if ( numOfPlayers < 32) {
+          setBracketWidth({
+            width: "90%"
+          })
+        }
+  }
+
+  const bs = {
+    display: "grid",
+    gridTemplateRows: `repeat(${numOfPlayers - 1}, 60px)`,
+    gridTemplateColumns: `repeat(${(2 * numOfRounds) + (numOfRounds - 1)}, 1fr)`,
+    gridTemplateAreas: generateTemplateAreas(numOfPlayers, numOfRounds),
+    justifyContent: "center",
+    alignItems: "stretch"
+  };
+
+
+  if (tournament !== null && numOfRounds !== 0) {
   return (
     <div key={tournament.id}>
       <div id="tournament-info">
@@ -101,179 +103,11 @@ const Tournament = () => {
         </div>
       </div>
 
-      <div id="tournament-bracket">
-        {/* <div id="tournament-rounds">
-          {[...Array(numOfRounds)].map((e, i) => 
-              <div className="round-details" key={i}>Round {i + 1}<div className="date" backgroundColor="red">{new Date(tournament.start_date).toString().slice(0, 10)}</div></div>
-          )}
-        </div> */}
-
-        <div id="tournament-games" style={{bracketStyle, ...bracketWidth, margin:"4rem auto",}}>
-          
-          {tournament.matches.map((match, i) => {
-            if(i < numOfPlayers / 2) {
-              return(
-
-                <div className={`round-1 game-${i + 1}`} style={{gridArea: `game-${i + 1}`}} key={i}>
-                  <section className="bracket">
-                    <div className="container">
-                      <div className="split split-one">
-                        <div className="round round-one current">
-                          <ul className="matchup">
-                            <li className="team team-top">{match.players[0].player_name}<span className="score">{match.players[0].score}</span></li>
-                            <li className="team team-bottom">{match.players[1].player_name}<span className="score">{match.players[1].score}</span></li>
-                          </ul>
-                        </div>  
-                      </div>  
-                    </div>  
-                  </section >
-                </div>
-
-              )
-            }
-
-            if(i >= numOfPlayers / 2 && i < (numOfPlayers - numOfPlayers / 4)) {
-
-              return(
-              <>
-                <div className="top_match_down" style={{gridArea:`top_match_down-${i}`}}>
-                  {top_match_down(numOfRounds)}
-                </div>
-                <div className="bottom_match_up" style={{gridArea:`bottom_match_up-${i}`}}>
-                  {bottom_match_up(numOfRounds)}
-                </div>
-                <div className="connector" style={{gridArea:`connector-${i}`}}>
-                  {connector(numOfRounds)}
-                </div>
-
-                <div className={`round-2 game-${i + 1}`} key={i} style={{gridArea: `game-${i + 1}`}}>
-                  <section className="bracket">
-                    <div className="container">
-                      <div className="split split-one">
-                        <div className="round round-one current">
-                          <ul className="matchup">
-                            <li className="team team-top">{match.players[0].player_name}<span className="score">{match.players[0].score}</span></li>
-                            <li className="team team-bottom">{match.players[1].player_name}<span className="score">{match.players[1].score}</span></li>
-                          </ul>
-                        </div>  
-                      </div>  
-                    </div>  
-                  </section >
-                </div>
-
-              </>
-              )
-            }
-
-            if(i >= (numOfPlayers - numOfPlayers / 4) && i < (numOfPlayers - numOfPlayers / 8)) {
-              return(
-              <>
-                <div className="top_match_down" style={{gridArea:`top_match_down-${i}`}}>
-                  {top_match_down(numOfRounds)}
-                </div>
-
-                <div className="bottom_match_up" style={{gridArea:`bottom_match_up-${i}`}}>
-                  {bottom_match_up(numOfRounds)}
-                </div>
-
-                <div className="connector" style={{gridArea:`connector-${i}`}}>
-                  {connector(numOfRounds)}
-                </div>
-
-                <div className="straight_line" style={{gridArea:`straight_line-${i}`}}>
-                  {straight_line(numOfRounds)}
-                </div>
-                <div className="straight_line2" style={{gridArea:`straight_line2-${i}`}}>
-                  {straight_line(numOfRounds)}
-                </div>
-
-                <div className={`round-3 game-${i + 1}`} key={i} style={{gridArea: `game-${i + 1}`}}>
-                  <section className="bracket">
-                    <div className="container">
-                      <div className="split split-one">
-                        <div className="round round-one current">
-                          <ul className="matchup">
-                            <li className="team team-top">{match.players[0].player_name}<span className="score">{match.players[0].score}</span></li>
-                            <li className="team team-bottom">{match.players[1].player_name}<span className="score">{match.players[1].score}</span></li>
-                          </ul>
-                        </div>  
-                      </div>  
-                    </div>  
-                  </section >
-                </div>
-              </>
-              )
-            }
-
-            if(i >= (numOfPlayers - numOfPlayers / 8) && i < (numOfPlayers - numOfPlayers / 16)) {
-              return(
-              <>
-                <div className="top_match_down" style={{gridArea:`top_match_down-${i}`}}>
-                {top_match_down(numOfRounds)}
-                </div>
-  
-                <div className="bottom_match_up" style={{gridArea:`bottom_match_up-${i}`}}>
-                  {bottom_match_up(numOfRounds)}
-                </div>
-  
-                <div className="connector" style={{gridArea:`connector-${i}`}}>
-                  {connector(numOfRounds)}
-                </div>
-
-                <div className="straight_line" style={{gridArea:`straight_line-${i}`}}>
-                  {straight_line(numOfRounds)}
-                </div>
-
-                <div className="straight_line2" style={{gridArea:`straight_line2-${i}`}}>
-                  {straight_line(numOfRounds)}
-                </div>
-
-                <div className="straight_line3" style={{gridArea:`straight_line3-${i}`}}>
-                  {straight_line(numOfRounds)}
-                </div>
-
-                <div className="straight_line4" style={{gridArea:`straight_line4-${i}`}}>
-                  {straight_line(numOfRounds)}
-                </div>
-
-                <div className="straight_line5" style={{gridArea:`straight_line5-${i}`}}>
-                  {straight_line(numOfRounds)}
-                </div>
-
-                <div className="straight_line6" style={{gridArea:`straight_line6-${i}`}}>
-                  {straight_line(numOfRounds)}
-                </div>
-
-
-                <div className={`round-4 game-${i + 1}`} key={i} style={{gridArea: `game-${i + 1}`}}>
-                  <section className="bracket">
-                    <div className="container">
-                      <div className="split split-one">
-                        <div className="round round-one current">
-                          <ul className="matchup">
-                            <li className="team team-top">{match.players[0].player_name}<span className="score">{match.players[0].score}</span></li>
-                            <li className="team team-bottom">{match.players[1].player_name}<span className="score">{match.players[1].score}</span></li>
-                          </ul>
-                        </div>  
-                      </div>  
-                    </div>  
-                  </section >
-                </div>
-              </>
-              )
-            }
-          })}
-
-        </div>  
-      </div>
+      <Bracket bs={bs} bracketWidth={bracketWidth} tournament={tournament} numOfPlayers={numOfPlayers} numOfRounds={numOfRounds}/>
+      
     </div>
-  ) } else {
-    return (
-      <div>
-        Loading...
-      </div>
-    )
-  }
+  )
+}
 }
 
 export default Tournament;
