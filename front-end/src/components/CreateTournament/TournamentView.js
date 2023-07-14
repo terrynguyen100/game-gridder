a838fab98575fd9937c1168df58f3c4217186e8e
Clayton <persinger.clayton@gmail.com>
2023-07-14 14:36:06 -0700

    Add bracket component and tourPlayerNum for props

--- front-end/src/components/CreateTournament/TournamentView.js
+++ front-end/src/components/CreateTournament/TournamentView.js
@@ -1,6 +1,7 @@
 import { Container, Paper, Input, Button, Box, Card, Typography, Grid } from "@mui/material";
 import { CreateTournamentContext } from "../../providers/CreateTournamentProvider";
 import { useContext, useEffect, useState } from "react";
+import Bracket from "../Tournament/Bracket";
 import axios from "axios";
 const TournamentView = () => {
   // ------------------- State & Context -------------------
@@ -8,7 +9,8 @@ const TournamentView = () => {
   const {
     tourMatches, setTourMatches,
     tourParticipants, setTourParticipants,
-    usersIds, setUsersIds
+    usersIds, setUsersIds,
+    tourPlayerNum, setTourPlayerNum,
   } = useContext(CreateTournamentContext);
 
   const updateMatches = () => {
@@ -49,38 +51,39 @@ const TournamentView = () => {
   }, [tourParticipants]);
 
   return (
-    <Box sx={{
-      maxWidth: '100%',
-      maxHeight: '100%',
-      border: '1px solid black',
-    }}>
-      <Grid container direction="row" justifyContent="center" alignItems="center">
-        {rounds.map((round, index) => (
-          <Grid item xs={3} sm={3} md={12 / rounds.length} key={index} sx={{ border: '1px solid black', padding: '10px' }}>
-            <Typography
-              variant="h2"
-              textAlign={'center'}
-            >
-              {round}
-            </Typography>
-          </Grid>
-        ))}
-      </Grid>
-      <Grid container direction="column" justifyContent="center" alignItems="center">
-        {tourMatches.map((match, index) => (
-          <Grid item xs={3} sm={3} md={12 / tourMatches.length} key={index} sx={{ border: '1px solid black' }}>
-            {match.players.map((player, index1) => (
-              <Typography
-                variant="h2"
-                textAlign={'center'}
-                key={index1}
-              > {player.player_name} </Typography>
-            ))}
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+    // <Box sx={{
+    //   maxWidth: '100%',
+    //   maxHeight: '100%',
+    //   border: '1px solid black',
+    // }}>
+    //   <Grid container direction="row" justifyContent="center" alignItems="center">
+    //     {rounds.map((round, index) => (
+    //       <Grid item xs={3} sm={3} md={12 / rounds.length} key={index} sx={{ border: '1px solid black', padding: '10px' }}>
+    //         <Typography
+    //           variant="h2"
+    //           textAlign={'center'}
+    //         >
+    //           {round}
+    //         </Typography>
+    //       </Grid>
+    //     ))}
+    //   </Grid>
+    //   <Grid container direction="column" justifyContent="center" alignItems="center">
+    //     {tourMatches.map((match, index) => (
+    //       <Grid item xs={3} sm={3} md={12 / tourMatches.length} key={index} sx={{ border: '1px solid black' }}>
+    //         {match.players.map((player, index1) => (
+    //           <Typography
+    //             variant="h2"
+    //             textAlign={'center'}
+    //             key={index1}
+    //           > {player.player_name} </Typography>
+    //         ))}
+    //       </Grid>
+    //     ))}
+    //   </Grid>
 
+    // </Box>
+    <Bracket numOfPlayers={tourPlayerNum} matchesObj={tourMatches}/>
   );
 };
 
