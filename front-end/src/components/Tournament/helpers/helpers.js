9e41da72df69c2cec57fc6c22e5dcbb11c4a8088
Clayton <persinger.clayton@gmail.com>
2023-07-16 16:36:57 -0700

    Update with edit grid generator

--- front-end/src/components/Tournament/helpers/helpers.js
+++ front-end/src/components/Tournament/helpers/helpers.js
@@ -88,4 +88,94 @@ function generateTemplateAreas(numOfPlayers) {
   return formattedTemplateAreas.join("\n");
 }
 
-module.exports = {generateTemplateAreas}
+function generateEditAreas(numOfPlayers) {
+  const numOfRounds = Math.ceil(Math.log2(numOfPlayers));
+  let arr = []
+
+  //3 matches
+  if (numOfRounds === 2) {
+    arr = [
+      ['name-1 game-1 game-1 . .'], 
+      ['. . name-3 game-3 game-3'], 
+      ['name-2 game-2 game-2 . .']
+    ] 
+  }
+  
+  //7 matches
+  if (numOfRounds === 3){
+    arr = [
+      ['name-1 game-1 game-1 . . . . .'], 
+      ['. . name-5 game-5 game-5 . . .'], 
+      ['name-2 game-2 game-2 . . . . .'],
+      ['. . . . name-7 game-7 game-7 .'], 
+      ['name-3 game-3 game-3 . . . . .'], 
+      ['. . name-6 game-6 game-6 . . .'],
+      ['name-4 game-4 game-4 . . . . .']
+    ]
+  }
+  
+  //15 matches
+  if (numOfRounds === 4){
+    arr = [
+      ['name-1 game-1 game-1 . . . . . . . .'], 
+      ['. . . name-9 game-9 game-9 . . . . .'], 
+      ['name-2 game-2 game-2 . . . . . . . .'],
+      ['. . . . . name-13 game-13 game-13 . . .'], 
+      ['name-3 game-3 game-3 . . . . . . . .'], 
+      ['. . . name-10 game-10 game-10 . . . . .'],
+      ['name-4 game-4 game-4 . . . . . . . .'],
+      ['. . . . . . . . name-15 game-15 game-15'],
+      ['name-5 game-5 game-5 . . . . . . . .'], 
+      ['. . . name-11 game-11 game-11 . . . . .'], 
+      ['name-6 game-6 game-6 . . . . . . . .'],
+      ['. . . . . name-14 game-14 game-14 . . .'],
+      ['name-7 game-7 game-7 . . . . . . . .'],
+      ['. . . name-12 game-12 game-12 . . . . .'],
+      ['name-8 game-8 game-8 . . . . . . . .']
+    ]
+  }
+
+  //31 matches
+  if (numOfRounds === 5){
+    arr = [
+      ['game-1 . . . .'], 
+      ['. game-17 . . .'], 
+      ['game-2 . . . .'],
+      ['. . game-25 . .'], 
+      ['game-3 . . . .'], 
+      ['. game-18 . . .'],
+      ['game-4 . . . .'],
+      ['. . . game-29 .'],
+      ['game-5 . . . .'], 
+      ['. game-19 . . .'], 
+      ['game-6 . . . .'],
+      ['. . game-26 . .'],
+      ['game-7 . . . .'],
+      ['. game-20 . . .'],
+      ['game-8 . . . .'],
+      ['. . . . game-31'], 
+      ['game-9 . . . .'],
+      ['. game-21 . . .'], 
+      ['game-10 . . . .'], 
+      ['. . game-27 . .'],
+      ['game-11 . . . .'],
+      ['. game-22 . . .'],
+      ['game-12 . . . .'], 
+      ['. . . game-30 .'], 
+      ['game-13 . . . .'],
+      ['. game-23 . . .'],
+      ['game-14 . . . .'],
+      ['. . game-28 . .'],
+      ['game-15 . . . .'],
+      ['. game-24 . . .'],
+      ['game-16 . . . .']
+    ]
+  }
+
+  const formattedTemplateAreas = arr.map((round) =>
+  `"${round.join(" ")}"`
+  );
+  return formattedTemplateAreas.join("\n");
+}
+
+module.exports = {generateTemplateAreas, generateEditAreas}
