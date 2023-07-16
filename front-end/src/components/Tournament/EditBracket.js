ac652362a1ca393faf48071a17110f12042f39d8
Clayton <persinger.clayton@gmail.com>
2023-07-16 10:57:03 -0700

    Functionality to advance users to next round and update database

--- front-end/src/components/Tournament/EditBracket.js
+++ front-end/src/components/Tournament/EditBracket.js
@@ -5,8 +5,8 @@ import axios from 'axios'
 const EditBracket = ({tournament, setTournament}) => {
   const [score, setScore] = useState([])
 
+  //Add entered scores to score state
   const handleChange = (e, playerIndex, matchIndex) => {
-
   const newScore = [...score]
   if ( newScore[matchIndex] ) {
     newScore[matchIndex][playerIndex] = e.target.value
@@ -14,23 +14,19 @@ const EditBracket = ({tournament, setTournament}) => {
     newScore[matchIndex] = {[playerIndex]: e.target.value }
   }
   setScore(newScore)
-
-
   }; 
 
+  //Update tournament state with new scores
   const submitScores = async(i, match) => {
-
     const player1Id = match.players[0].id
     const player2Id = match.players[1].id
     const player1Score = Number(score[i][0])
     const player2Score = Number(score[i][1])
 
-    console.log("ids: ",player1Id, player2Id)
-    console.log("scores: ",score[i][0], score[i][1])
+    await axios.patch(`/players/${player1Id}`, {score: player1Score})
+    await axios.patch(`/players/${player2Id}`, {score: player2Score})
 
-    const updatedScore1 = await axios.patch(`/players/${player1Id}`, {score: player1Score})
-    const updatedScore2 = await axios.patch(`/players/${player2Id}`, {score: player2Score})
-
+    //Update score in tournament state
     const newMatches = [...tournament.matches]
     newMatches[i].players[0].score = score[i][0];
     newMatches[i].players[1].score = score[i][1];
@@ -40,6 +36,39 @@ const EditBracket = ({tournament, setTournament}) => {
         matches: newMatches
       }
     })
+
+    const winningPlayer = player1Score > player2Score ? {...match.players[0]} : {...match.players[1]}
+    
+    for(match in tournament.matches) {
+
+      if (tournament.matches[match]?.players.length === 0 || tournament.matches[match]?.players.length === 1) {
+        //Update tournament state  with new player
+        //Reset user's score before updating state
+        const newPlayers = [...tournament.matches]
+        if(tournament.matches[match]?.players.length === 0){
+          newPlayers[match].players[0] = winningPlayer;
+          newPlayers[match].players[0].score = ''
+        } else {
+          newPlayers[match].players[1] = winningPlayer;
+          newPlayers[match].players[1].score = ''
+        }
+        setTournament(prev => {
+          return{
+            ...prev,
+            matches: newPlayers
+          }
+        })
+
+        updateMatch(tournament.matches[match].id, winningPlayer)
+        break
+      }
+    }
+  }
+
+  //Updates match database with winning user
+  const updateMatch = async(match_id, winningPlayer) => {
+    const player_name = winningPlayer.player_name
+    await axios.post(`/players/`, {match_id, player_name})
   }
 
   return(
@@ -53,31 +82,31 @@ const EditBracket = ({tournament, setTournament}) => {
         autoComplete="off"
       >
         {tournament.matches.map((match, i) => {
-          return(
-            <>
-              <div className="name-score-container">
-                <div className="edit-name-score">
-                  <div className="player">
-                    <h1 className="edit-name">{match?.players[0]?.player_name}</h1>
-                    <FormControl fullWidth >
-                      <TextField id="outlined-basic" name="0" label="Score" variant="outlined" onChange={(e) => handleChange(e, 0, i)}/>
-                    </FormControl>
+          if(tournament.matches[i].players.length === 2) {
+            return(
+              <>
+                <div className="name-score-container">
+                  <div className="edit-name-score">
+                    <div className="player">
+                      <h1 className="edit-name">{match?.players[0]?.player_name}</h1>
+                      <FormControl fullWidth  >
+                        <TextField id="outlined-basic" name="0" label="Score" variant="outlined" onChange={(e) => handleChange(e, 0, i)} />
+                      </FormControl>
+                    </div>
+  
+                    <div className="player">
+                      <h1 className="edit-name">{match?.players[1]?.player_name}</h1>
+                      <FormControl fullWidth >
+                        <TextField id="outlined-basic" name="1" label="Score" variant="outlined" onChange={(e) => handleChange(e, 1, i)} />
+                      </FormControl>     
+                    </div>
+   
                   </div>
-
-                  <span>Vs.</span>
-
-                  <div className="player">
-                    <h1 className="edit-name">{match?.players[1]?.player_name}</h1>
-                    <FormControl fullWidth >
-                      <TextField id="outlined-basic" name="1" label="Score" variant="outlined" onChange={(e) => handleChange(e, 1, i)}/>
-                    </FormControl>     
-                  </div>
- 
+                  <Button variant="contained" onClick={() => submitScores(i, match)}>Submit</Button>
                 </div>
-                <Button variant="contained" onClick={() => submitScores(i, match)}>Submit</Button>
-              </div>
-            </>
-          )
+              </>
+            )
+          }
         })}
       </Box>
 
