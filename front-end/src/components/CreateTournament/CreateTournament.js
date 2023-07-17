a966f1f72a781e6cfac3d23db2565e20a353e900
Terry Nguyen <terrynguyen100@gmail.com>
2023-07-17 11:44:35 -0600

    redirect user to login page when

--- front-end/src/components/CreateTournament/CreateTournament.js
+++ front-end/src/components/CreateTournament/CreateTournament.js
@@ -1,8 +1,20 @@
 import "./../../sass/createTournaments.scss";
 import CreateTournamentProvider from "../../providers/CreateTournamentProvider";
 import LeftDrawer from "./LeftDrawer";
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from "../../providers/AuthProvider";
+import { useContext, useEffect } from "react";
 
 const CreateTournament = () => {
+  const { userId } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userId) {
+      navigate('/login');
+    }
+  }, [userId]);
+
   return (
     <CreateTournamentProvider>
       <LeftDrawer/>
